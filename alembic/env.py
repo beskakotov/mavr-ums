fe605992e1a3e4030b0baeb73fd6c2b770bfe7f4
Anatoly Beskakotov <beskakotov.as@gmail.com>
2022-03-20 16:29:48 +0300

    Рефакторинг пакета. Настройка alembic
    Настроен alembic под работу с keyring. В случае ошибки подключения - в консоли отображается сообщение о введении новых данных подключения.
    Изменено дерево пакета. Все GUI постепенно будут переноситься в соответствующий модуль, а остальные будут располагаться в корне.

--- alembic/env.py
+++ alembic/env.py
@@ -1,16 +1,61 @@
 from logging.config import fileConfig
+from matplotlib.style import use
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+import psycopg2
+from getpass import getpass
 
 from alembic import context
+
+import keyring
+
+def get_keyring_values(force_new=False):
+    def validate_parameters():
+        username = keyring.get_password('ums.sima', 'username')
+        password = keyring.get_password('ums.sima', 'password')
+        host = keyring.get_password('ums.sima', 'host')
+        port = keyring.get_password('ums.sima', 'port')
+        dbname = keyring.get_password('ums.sima', 'dbname')
+        try:
+            connection = psycopg2.connect(
+                dbname=dbname,
+                user=username,
+                password=password,
+                host=host,
+                port=port
+            )
+        except:
+            return get_keyring_values(force_new=True)
+        else:
+            return dict(DB_USER=username, DB_PASS=password, DB_HOST=host, DB_PORT=port, DB_NAME=dbname)
+
+
+    username = keyring.get_password('ums.sima', 'username')
+    if force_new or username is None:
+        if force_new and username is not None:
+            print('Error in login data. Please, input new.')
+        keyring.set_password('ums.sima', 'username', input('Enter database username: '))
+        keyring.set_password('ums.sima', 'password', getpass('Enter database password: '))
+        keyring.set_password('ums.sima', 'host', input('Enter database host: '))
+        keyring.set_password('ums.sima', 'port', input('Enter database port: '))
+        keyring.set_password('ums.sima', 'dbname', input('Enter database name: '))
+        return validate_parameters()
+    else:
+        return validate_parameters()
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 section = config.config_ini_section
-config.set_section_option(section, "SIMA_USER", os.environ.get("SIMA_USER"))
-config.set_section_option(section, "SIMA_PASS", os.environ.get("SIMA_PASS"))
+
+db_data = get_keyring_values()
+
+config.set_section_option(section, "DB_USER", db_data['DB_USER'])
+config.set_section_option(section, "DB_PASS", db_data['DB_PASS'])
+config.set_section_option(section, "DB_HOST", db_data['DB_HOST'])
+config.set_section_option(section, "DB_PORT", db_data['DB_PORT'])
+config.set_section_option(section, "DB_NAME", db_data['DB_NAME'])
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -21,7 +66,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from ums import sima
+target_metadata = sima.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
---
+++ setup.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+packages = find_packages()
+# ['ums',
+#  'ums.common',
+#  'ums.orvi',
+#  'ums.sima',
+#  'ums.sima.common',
+#  'ums.sima.widgets',
+#  'ums.sima.widgets.add']
+
+package_data = \
+{'': ['*']}
+
+install_requires = [] #\
+# ['PySide2>=5.15.2,<6.0.0',
+#  'SQLAlchemy>=1.4.23,<2.0.0',
+#  'alembic>=1.7.1,<2.0.0',
+#  'keyring>=23.2.1,<24.0.0',
+#  'matplotlib>=3.4.3,<4.0.0',
+#  'numpy>=1.21.2,<2.0.0',
+#  'pandas>=1.3.3,<2.0.0',
+#  'psycopg2>=2.9.1,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['mavr-orvi = ums.orvi:run', 'mavr-sima = ums.sima:run']}
+
+setup_kwargs = {
+    'name': 'ums',
+    'version': '0.0.1',
+    'description': 'Will be...',
+    'long_description': None,
+    'author': 'Anatoly Beskakotov',
+    'author_email': 'beskakotov.as@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9',
+}
+
+
+setup(**setup_kwargs)
